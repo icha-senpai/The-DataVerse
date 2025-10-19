9b4e6fcdd451964ef082dda663fcff0549e6c8a5
Icha <footmouthkick@gmail.com>
2025-10-19 12:17:00 -0500

    localized fonts

--- octobertest2/storage/cms/twig/eb/eb46785afad15bc43e69fdc895bf13d1.php
+++ octobertest2/storage/cms/twig/eb/eb46785afad15bc43e69fdc895bf13d1.php
@@ -44,54 +44,53 @@ class __TwigTemplate_b7f92ef209b309d5aec492b23def9671 extends Template
   <head>
     <meta charset=\"utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-    <link href=\"https://fonts.googleapis.com/css2?family=Tilt+Neon:wght@400;700&display=swap\" rel=\"stylesheet\">
     <title>";
-        // line 7
-        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Cms\Twig\GetAttrNode::customGetAttribute($this->env, $this->source, Cms\Twig\GetAttrNode::customGetAttribute($this->env, $this->source, ($context["this"] ?? null), "page", [], "any", false, false, false, 7), "title", [], "any", false, false, false, 7), "html", null, true);
+        // line 6
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Cms\Twig\GetAttrNode::customGetAttribute($this->env, $this->source, Cms\Twig\GetAttrNode::customGetAttribute($this->env, $this->source, ($context["this"] ?? null), "page", [], "any", false, false, false, 6), "title", [], "any", false, false, false, 6), "html", null, true);
         yield " - The DataVerse</title>
 
     ";
-        // line 9
+        // line 8
         $_minify = System\Classes\CombineAssets::instance()->useMinify;
         yield '<script src="' . Request::getBasePath() . '/modules/system/assets/js/framework-extras'.($_minify ? '.min' : '').'.js"></script>' . PHP_EOL;
         yield '<link rel="stylesheet" property="stylesheet" href="' . Request::getBasePath() .'/modules/system/assets/css/framework-extras.css">' . PHP_EOL;
         unset($_minify);
-        // line 10
+        // line 9
         yield "    ";
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->assetsFunction('css');
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->displayBlock('styles');
-        // line 11
+        // line 10
         yield "    <link rel=\"stylesheet\" href=\"";
         yield $this->extensions['Cms\Twig\Extension']->themeFilter("assets/vendor/tailwind/tailwind.css");
         yield "\">
   </head>
 
-  <body class=\"bg-dv-bg text-dv-text min-h-screen flex flex-col\">
+  <body class=\"bg-dv-bg text-dv-text min-h-screen flex flex-col font-tilt\">
 
     ";
-        // line 16
+        // line 15
         $cmsPartialParams = [];
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->partialFunction("site/navbar"        , array_merge($context, ['__cms_partial_params' => $cmsPartialParams], $cmsPartialParams)        , true);
-        // line 17
+        // line 16
         yield "
     <main class=\"flex-grow flex items-center justify-center container mx-auto px-4\">
       ";
-        // line 19
+        // line 18
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->pageFunction($context);
-        // line 20
+        // line 19
         yield "    </main>
 
     ";
-        // line 22
+        // line 21
         $cmsPartialParams = [];
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->partialFunction("site/footer"        , array_merge($context, ['__cms_partial_params' => $cmsPartialParams], $cmsPartialParams)        , true);
-        // line 23
+        // line 22
         yield "
     ";
-        // line 24
+        // line 23
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->assetsFunction('js');
         yield $this->env->getExtension(\Cms\Twig\Extension::class)->displayBlock('scripts');
-        // line 25
+        // line 24
         yield "  </body>
 </html>";
         yield from [];
@@ -118,7 +117,7 @@ class __TwigTemplate_b7f92ef209b309d5aec492b23def9671 extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  95 => 25,  92 => 24,  89 => 23,  86 => 22,  82 => 20,  80 => 19,  76 => 17,  73 => 16,  64 => 11,  60 => 10,  55 => 9,  50 => 7,  42 => 1,);
+        return array (  94 => 24,  91 => 23,  88 => 22,  85 => 21,  81 => 19,  79 => 18,  75 => 16,  72 => 15,  63 => 10,  59 => 9,  54 => 8,  49 => 6,  42 => 1,);
     }
 
     public function getSourceContext(): Source
@@ -128,7 +127,6 @@ class __TwigTemplate_b7f92ef209b309d5aec492b23def9671 extends Template
   <head>
     <meta charset=\"utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-    <link href=\"https://fonts.googleapis.com/css2?family=Tilt+Neon:wght@400;700&display=swap\" rel=\"stylesheet\">
     <title>{{ this.page.title }} - The DataVerse</title>
 
     {% framework extras %}
@@ -136,7 +134,7 @@ class __TwigTemplate_b7f92ef209b309d5aec492b23def9671 extends Template
     <link rel=\"stylesheet\" href=\"{{ 'assets/vendor/tailwind/tailwind.css'|theme }}\">
   </head>
 
-  <body class=\"bg-dv-bg text-dv-text min-h-screen flex flex-col\">
+  <body class=\"bg-dv-bg text-dv-text min-h-screen flex flex-col font-tilt\">
 
     {% partial 'site/navbar' %}
 
